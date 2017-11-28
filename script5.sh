c780414be4a93c9ac798869222abe320e2be2a30
lior cohen <pppor22@gmail.com>
2017-11-28 04:39:11 +0200

    home work 26.11 + organized directorys

--- script5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-##create a file that will run "mkdir" with parenthood to folder that is named after you. after it is created change your location in to it
-
-#make parent diractory with my name
-mkdir -p lior && cd lior
-
-#pwd to chack that "cd" commend work
-pwd
--- script6.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-##create a file that runs "find" commands and seeks all the files with "tmp" in its name.
-
-#find all file with "tmp" in the name and send errors to null.
-find / -type f -name "tmp" 2>/dev/null
-
-
