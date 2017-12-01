214f8e835560ffb53eb2bb2a1031cd1485727ae5
lior cohen <pppor22@gmail.com>
2017-12-01 02:53:26 +0200

    homework 7-26.11

--- hw-26.11/script7-df.sh
+++ hw-26.11/script7-df.sh
@@ -3,14 +3,10 @@
 #date: 28/11/2017
 #name: lior cohen
 #homework: 7. create a script that will use "df" command and "for" loop will print only disks with low space.
-<< eof
-df | awk -F " " '{print $5}'  
-echo $#
 
-for ((i=0; i<=; i++)); do
-	if [[ $5 >=40% ]]; then
-	echo "$1 low disk space!"
-	else echo ""
+
+for i in $(df -h | awk '{print $5}' | sed {s/%/" "/g}); do
+	if [[ $i -gt 40 ]];then
+		echo "low space ${i}%";
 fi
 done
-	eof
